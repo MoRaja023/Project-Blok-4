20abff2def151fc25932294c6e7e2e69c1e8a0fc
Mohamed El Harchaui <melharchaui784@gmail>
2023-06-07 09:48:17 +0200

    database configuratie

---
+++ site/database.php
@@ -0,0 +1,10 @@
+<?php
+// Database configuratie
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "password";
+$dbname = "projectblok4";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+?>
