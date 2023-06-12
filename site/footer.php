d9adf35504552631fa628254a1d5d1fa1f9eeca1
Mohamed El Harchaui <melharchaui784@gmail>
2023-06-12 13:14:55 +0200

    footer.php en footer.css toegevoegd

--- site/footer.php
+++ site/footer.php
@@ -5,21 +5,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
     <title>footer</title>
 </head>
 
 <body>
-    <footer>
-        <div class="footer1">
-            <h3>Student</h3>
-            <p>2023 Mohamed - 155784</p>
-        </div>
-        <div class="footer2">
-            <h3>Over</h3>
-            <p>Nova College</p>
-            <div class="footer3">
+    <footer class="flex-container1">
+        <div class="footer">
+            <div class="footer1">
+                <h3>Student</h3>
+                <p>2023 Mohamed - 155784</p>
+            </div>
+
+            <div class="footer1">
+                <h3>Over</h3>
+                <div class="footer2">
+                    <p>Nova College</p>
+                </div>
+            </div>
+
+            <div class="footer1">
                 <h3>Contact</h3>
-                <p>melharchaui784@student.novacollege.nl</p>
+                <div class="footer2">
+                    <p>melharchaui784@student.novacollege.nl</p>
+                </div>
             </div>
         </div>
     </footer>
